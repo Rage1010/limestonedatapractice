656ea7d207e9ac32cacdd3899aa587d420558aae
Raghav Goyal <129891484+Rage1010@users.noreply.github.com>
2024-09-30 22:59:15 +0530

    The final challenge files

--- task1_kmeans.py
+++ task1_kmeans.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 from sklearn import datasets # to retrieve the iris Dataset
 import pandas as pd # to load the dataframe
 from sklearn.preprocessing import StandardScaler # to standardize the features
@@ -6,18 +5,23 @@ from sklearn.decomposition import PCA # to apply PCA
 from sklearn.cluster import KMeans
 import seaborn as sns # to plot the heat maps
 import matplotlib.pyplot as plt
+from dict import players_trust_list
 
-df = pd.read_csv("returns.csv")
-df = df.transpose()
-# print(df.head())
+
+dataf = pd.DataFrame(players_trust_list , columns=["0" , "TC" , "CC" , "TT" , "CT" , "titfortat" , "trustperc" , "0" , "1" , "2","3","4","5","6","7"])
+# print(dataf)
+# heat_map = sns.heatmap(dataf.corr())
+# plt.show()
+
+
 
 #Standardize the features
 #Create an object of StandardScaler which is present in sklearn.preprocessing
 scalar = StandardScaler() 
-scaled_data = pd.DataFrame(scalar.fit_transform(df)) #scaling the data
+scaled_data = pd.DataFrame(scalar.fit_transform(dataf)) #scaling the data
 # print(scaled_data)
-
-# sns.heatmap(scaled_data.corr())
+# heat_map = sns.heatmap(scaled_data.corr())
+# plt.show()
 
 #Applying PCA
 #Taking no. of Principal Components as {n_components}
@@ -28,9 +32,9 @@ pca_columns = [f'PC{i}' for i in range(1,n_components+1)]
 pca.fit(scaled_data)
 data_pca = pca.transform(scaled_data)
 data_pca = pd.DataFrame(data_pca,columns=pca_columns)
-print(data_pca.head())
-heat_map = sns.heatmap(data_pca.corr())
-plt.show()
+# print(data_pca.head())
+# heat_map = sns.heatmap(data_pca.corr())
+# plt.show()
 
 
 #initialize kmeans parameters
@@ -42,46 +46,35 @@ kmeans_kwargs = {
 
 #create list to hold SSE values for each k
 sse = []
-for k in range(1, 11):
+r = range(2, 15)
+for k in r:
     kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
     kmeans.fit(data_pca)
     sse.append(kmeans.inertia_)
 
 #visualize results
-plt.plot(range(1, 11), sse)
-plt.xticks(range(1, 11))
+plt.plot(r, sse)
+plt.xticks(r)
 plt.xlabel("Number of Clusters")
 plt.ylabel("SSE")
 plt.show()
 
-n_clusters = 4
+n_clusters = 14
 #instantiate the k-means class, using optimal number of clusters
 kmeans = KMeans(init="random", n_clusters=n_clusters, n_init=10, random_state=1)
 
 #fit k-means algorithm to data
 kmeans.fit(data_pca)
-df['cluster'] = kmeans.labels_
+dataf['cluster'] = kmeans.labels_
+# print(kmeans.labels_)
 # print(df)
 # print(data_pca)
-
-# inertia_values = []
-# inertia_values.append(kmeans.inertia_)
-# y_kmeans = kmeans.fit_predict(data_pca)
-# inertia_values.append(kmeans.inertia_)
-# plt.scatter(data_pca.iloc[:, 'PC1'], data_pca.iloc[:, 'PC2'], c=y_kmeans)
-# plt.scatter(kmeans.cluster_centers_[:, 0],\
-#                 kmeans.cluster_centers_[:, 1], \
-#                 s=100, c='red')
-# plt.title('K-means clustering (k={})'.format(n_clusters))
-# plt.xlabel('Feature 1')
-# plt.ylabel('Feature 2')
-# plt.show()
 
 
 
 lulu = [[] for i in range(n_clusters)]
-for ind in df.index:
-    lulu[int(df["cluster"][ind])].append(ind)
+for ind in dataf.index:
+    lulu[int(dataf["cluster"][ind])].append(ind)
 for i in range(n_clusters):
     print(lulu[i])
     print(len(lulu[i]))
